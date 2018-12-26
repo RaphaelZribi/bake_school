bc6fa6dfa91dfa22721a2e83f46774ca88c5f586
RaphZribi <raphaelzribi1@gmail.com>
2018-12-26 16:18:45 +0200

    sessions page and description ready

--- bakin_school/class_schedule_app/urls.py
+++ bakin_school/class_schedule_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'class_schedule_app'
 
 urlpatterns = [
+	path('all_sessions/', views.all_sessions, name='all_sessions'),
+	path('session_description/<int:session_id>/', views.session_description, name='session_description'),
 	path('new_session/', views.new_session, name="new_session"),
 ]
 
--- bakin_school/class_schedule_app/views.py
+++ bakin_school/class_schedule_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from class_schedule_app.forms import SessionForm
+from class_schedule_app.models import Session
 
 
+def all_sessions(request):
+	sessions = Session.objects.all()
+	return render(request, 'all_sessions.html', {'sessions': sessions})
+
+def session_description(request, session_id):
+	session = Session.objects.get(id=session_id)
+	return render (request, 'session_description.html', {'session':session})
 
 def new_session(request):
 	if request.method =='POST':	
@@ -10,8 +17,7 @@ def new_session(request):
 		if form.is_valid():
 			session = form.save()
 			if 'image' in request.FILES:
-				form.image = request.FILES['image']
-			
+				form.image = request.FILES['image']	
 			return HttpResponse('New Class Created!')
 	else: 
 		form = SessionForm()
